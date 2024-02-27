6cf21dff0fa7fac06e9c8ff5a30792bd21704f45
장준혁 <144774186+jh990714@users.noreply.github.com>
2024-02-28 01:41:25 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 11.30 ms, Memory: 13.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.py
@@ -0,0 +1,39 @@
+from collections import deque
+import heapq
+
+direction = [(1, 0, 'd'), (0, -1, 'l'), (0, 1, 'r'), (-1, 0, 'u')]
+
+def bfs(n, m, x, y, r, c, k):
+    q = []
+    heapq.heappush(q, ("", x, y))
+
+    while q:
+        path, x, y = heapq.heappop(q)
+        
+        
+        if (x, y) == (r, c):
+            if len(path) == k:
+                return path
+            
+            if (k - len(path)) % 2 == 1:
+                return "impossible"
+            
+        for dx, dy, s in direction:
+            nx = x + dx
+            ny = y + dy
+
+            if 1 <= nx <= n and 1 <= ny <= m: 
+                distance = abs(r - nx) + abs(c - ny)
+                if distance > (k - len(path) - 1):
+                    continue
+                    
+                heapq.heappush(q, (path+s, nx, ny))
+
+    return "impossible"
+
+def solution(n, m, x, y, r, c, k):
+    answer = bfs(n, m, x, y, r, c, k)
+
+    return answer
+
+print(solution(3, 4, 2, 3, 3, 1, 5))
