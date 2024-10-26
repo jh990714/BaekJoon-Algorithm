af4fa7e7fb17b4508ae84976f060ebd2ceec2c9e
장준혁 <144774186+jh990714@users.noreply.github.com>
2024-10-27 02:01:12 +0900

    [Gold III] Title: 감시, Time: 468 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.py
+++ 백준/Gold/15683. 감시/감시.py
@@ -1,78 +1,61 @@
-import sys
+# 방향 벡터 정의 (하, 우, 상, 좌)
+directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
-input = sys.stdin.readline
-import copy
-
-dirctions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-cctv_directions =  [
-    [[0], [1], [2], [3]],
-    [[0, 2], [1, 3]],
-    [[0, 1], [1, 2], [2, 3], [0, 3]],
-    [[0, 1, 2], [0, 1, 3], [1, 2, 3], [0, 2, 3]],
-    [[0, 1, 2, 3]],
+# CCTV 종류별 감시 방향 조합 정의
+cctv_directions = [
+    [[0], [1], [2], [3]],  # 1번 CCTV
+    [[0, 2], [1, 3]],      # 2번 CCTV
+    [[0, 1], [1, 2], [2, 3], [0, 3]],  # 3번 CCTV
+    [[0, 1, 2], [1, 2, 3], [2, 3, 0], [0, 1, 3]],  # 4번 CCTV
+    [[0, 1, 2, 3]],        # 5번 CCTV (모든 방향 감시)
 ]
 
-def dfs(idx, tlst):
+def watch(x, y, direction, graph):
+    """CCTV가 주어진 방향으로 감시하는 함수 (상태 변경)"""
+    for d in direction:
+        nx, ny = x, y
+        while True:
+            nx += directions[d][0]
+            ny += directions[d][1]
+            # 범위를 벗어나거나 벽(6)을 만나면 중단
+            if nx < 0 or nx >= n or ny < 0 or ny >= m or graph[nx][ny] == 6:
+                break
+            # 빈 칸(0)만 감시 표시
+            if graph[nx][ny] == 0:
+                graph[nx][ny] = '#'
+
+def dfs(depth):
+    """DFS 탐색과 백트래킹을 사용한 최적화된 CCTV 배치 탐색"""
     global answer
 
-    if idx == len(cctvs):
-        answer = min(answer, zero_cnt(tlst))
+    if depth == len(cctvs):  # 모든 CCTV를 배치한 경우 남은 0의 개수 계산
+        count = sum(row.count(0) for row in room)
+        answer = min(answer, count)
         return
 
+    # 현재 CCTV의 정보 가져오기
+    cctv_type, x, y = cctvs[depth]
 
-    level, x, y = cctvs[idx]
+    original = [row[:] for row in room]  # 백트래킹을 위해 원본 저장
 
-    for cctv_direction in cctv_directions[level]:
-        dfs(idx+1, tlst + [cctv_direction])
+    # 해당 CCTV의 가능한 모든 방향 조합에 대해 탐색
+    for direction in cctv_directions[cctv_type]:
+        watch(x, y, direction, room)  # 감시 실행
+        dfs(depth + 1)  # 다음 CCTV 배치
+        room[:] = [row[:] for row in original]  # 백트래킹 (원본 복구)
 
-        
-def zero_cnt(tlst):
-    room_copy = copy.deepcopy(room)
-
-    for cctv, cctv_direction in zip(cctvs, tlst):
-        
-
-        for d in cctv_direction:
-            # print(tlst, d, dirctions[d])
-            _, nx, ny = cctv
-            dx, dy = dirctions[d]
-
-            while True:
-                nx += dx
-                ny += dy
-
-                if not (0 <= nx < n and 0 <= ny < m):
-                    break
-
-                if room_copy[nx][ny] == 6:
-                    break
-
-                room_copy[nx][ny] = '#'
-        
-            # for i in room_copy:
-            #     print(i)
-            # print()
-
-    cnt = 0
-    for i in range(n):
-        cnt += room_copy[i].count(0)
-
-    return cnt
-
+# 입력 처리
 n, m = map(int, input().split())
-
 room = []
 cctvs = []
-answer = float('inf')
 
 for i in range(n):
     row = list(map(int, input().split()))
     room.append(row)
+    for j in range(m):
+        if 1 <= row[j] <= 5:  # CCTV 발견 시 리스트에 추가
+            cctvs.append((row[j] - 1, i, j))
 
-    for j in range(len(row)):
-        if 1 <= row[j] <= 5:
-            cctvs.append((row[j]-1, i, j))
-
-
-dfs(0, [])
-print(answer)
+answer = float('inf')  # 최소 사각지대 크기 초기화
+dfs(0)  # DFS 탐색 시작
+print(answer)  # 결과 출력
