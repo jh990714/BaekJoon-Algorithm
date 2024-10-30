533cbc2914b720e9a4eeff427a2d913283bbab8f
장준혁 <144774186+jh990714@users.noreply.github.com>
2024-10-31 03:13:04 +0900

    [Gold V] Title: 동전 2, Time: 372 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -0,0 +1,11 @@
+n, k = map(int, input().split())
+coins = [int(input()) for _ in range(n)]
+
+dp = [10001] * (k+1)
+dp[0] = 0
+
+for coin in coins:
+    for i in range(coin, k+1):
+        dp[i] = min(dp[i], dp[i-coin] + 1)
+
+print(dp[-1] if dp[-1] != 10001 else -1)
