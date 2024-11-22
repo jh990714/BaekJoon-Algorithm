eb6dd51d88d7d3b35ea0b5515c68938a62eb9eda
장준혁 <144774186+jh990714@users.noreply.github.com>
2024-11-23 04:27:54 +0900

    [level 3] Title: 순위, Time: 11.48 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int solution(int n, int[][] results) {
+        int[][] graph = new int[n+1][n+1];
+        
+        for (int i = 0; i < results.length; i++) {
+            int u = results[i][0];
+            int v = results[i][1];
+            
+            graph[u][v] = 1;
+            graph[v][u] = -1;
+        }
+        
+        // 시작
+        for (int k = 1; k <= n; k++) {
+            // 끝
+            for (int j = 1; j <= n; j++) {
+                // 중간
+                for (int i = 1; i <= n; i++) {
+                    if (graph[i][k] == 1 && graph[k][j] == 1) {
+                        graph[i][j] = 1;
+                        graph[j][i] = -1;
+                    }
+                    
+                    if (graph[i][k] == -1 && graph[k][j] == -1) {
+                        graph[i][j] = -1;
+                        graph[j][i] = 1;
+                    }
+                }
+            }
+        }
+        
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            int cnt = 0;
+            for (int j = 1; j <= n; j++) {
+                if (graph[i][j] != 0) {
+                    cnt++;
+                }
+            }
+            
+            if (cnt == n-1) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
